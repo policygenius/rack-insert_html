c89c2bf8ba1d98f1f5805f176d2ca3199a52beb5
Ian Yamey <ian@policygenius.com>
2015-03-19 17:48:47 -0700

    Insert content to every response at the insertion_point

--- lib/rack/insert_html.rb
+++ lib/rack/insert_html.rb
@@ -1,16 +1,25 @@
 module Rack
   class InsertHtml
     VERSION = '0.0.1'
+    DEFAULT_INSERTION_POINT = '</body>'
 
     def initialize(app, options = {})
-      @app = app
+      @app, @options = app, options
+      @options[:insertion_point] ||= DEFAULT_INSERTION_POINT 
     end
 
     def call(env)
       response = @app.call(env)
       status, headers, body = response
-      headers['Content-Length']= body.length.to_s
-      [status, headers, body]
+
+      # find the last matching insertion point in the body and insert the content
+      pre, match, post = body.first.rpartition(@options[:insertion_point])
+      body = "#{pre}#{@options[:content]}#{match}#{post}"
+
+      # Update the content-length
+      headers['Content-Length'] = body.bytesize.to_s
+
+      [status, headers, [body]]
     end
   end
 end
--- spec/rack/insert_html_spec.rb
+++ spec/rack/insert_html_spec.rb
@@ -4,18 +4,57 @@ require 'rack/lint'
 require 'rack/mock'
 
 describe Rack::InsertHtml do
-  let(:app) { proc { |env| [200, {'Content-Type' => 'text/plain'}, ['Hello']] } }
-  let(:stack) { Rack::InsertHtml.new(app) }
+  let(:example_html) { "<html><head></head><body><h1>Example Page</h1></body></html>" }
+  let(:inner_app) do
+    proc { |env| 
+      [
+        200, 
+        { 
+          'Content-Type' => 'text/html', 
+          'Content-Length' => example_html.bytesize 
+        }, 
+        [example_html]
+      ]
+    }
+  end
   let(:request)  { Rack::MockRequest.new(Rack::Lint.new(stack)) }
 
   it 'has a version number' do
     expect(Rack::InsertHtml::VERSION).not_to be nil
   end
 
-  it 'is a valid rack app' do
-    linted_app = Rack::Lint.new(stack)
-    env = Rack::MockRequest.env_for('/')
-    linted_app.call(env)
+  context 'without any custom configuration' do
+    let(:stack) { Rack::InsertHtml.new(inner_app) }
+
+    it 'is a valid rack app' do
+      request.get '/'
+    end
+
+    it 'does not append anything' do
+      response = request.get '/'
+      expect(response.body).to eq example_html
+    end
+  end
+
+  context 'with configuration for content' do
+    let(:content) { '<script>var insertedContent = 123;</script>' }
+    let(:options) { { content: content } }
+    let(:stack) { Rack::InsertHtml.new(inner_app, options) }
+
+    it 'appends the content to the <body> of the response' do
+      response = request.get '/'
+      expect(response.body).to include "#{content}</body>"
+    end
+
+    context 'and configuration for the insertion point' do
+      let(:insertion_point) { '</head>' }
+      let(:options) { { content: content, insertion_point: insertion_point } }
+
+      it 'appends the content before the custom insertion point' do
+        response = request.get '/'
+        expect(response.body).to include "#{content}#{insertion_point}"
+      end
+    end
   end
 end
 
